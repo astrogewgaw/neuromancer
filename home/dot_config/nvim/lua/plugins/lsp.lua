7282a52ede06212c2fd9ec6f9b9767a60f6e2750
Ujjwal Panda <ujjwalpanda97@gmail.com>
2023-02-13 19:50:20 +0530

    :bug: Fix Julia LSP.

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -6,9 +6,17 @@ return {
       bashls = {},
       clangd = {},
       texlab = {},
-      julials = {},
+      pyright = {},
       marksman = {},
       rust_analyzer = {},
+      julials = {
+        on_new_config = function(new_config, _)
+          local julia = vim.fn.expand("~/.julia/environments/lsp/bin/julia")
+          if require("lspconfig").util.path.is_file(julia) then
+            new_config.cmd[1] = julia
+          end
+        end,
+      },
     },
   },
 }
